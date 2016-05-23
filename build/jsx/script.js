79a022fd53033620cf532ccd1e731dcf88dc65a5
Preschian Febryantara <preschian27@gmail.com>
2016-05-23 17:35:49 +0700

    update package

--- build/jsx/script.js
+++ build/jsx/script.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 export class App extends React.Component {
     render() {
-        return <h2>Compile Sass + ReactJS using Gulp</h2>;
+        return <h2>Compile Sass + ReactJS using Gulp 😁</h2>;
     }
 }
 
